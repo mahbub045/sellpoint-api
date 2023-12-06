025b04298ee6b244996d0185de3b18eafb8f52f0
mahbub045 <mahbubrmn45@gmail.com>
2023-12-06 16:07:33 +0600

    change variable name

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema(
     {
-        fullName: { type: String, required: true },
+        name: { type: String, required: true },
         phone: { type: String, required: true, unique: true },
         email: { type: String, required: true, unique: true },
         password: { type: String, required: true },
